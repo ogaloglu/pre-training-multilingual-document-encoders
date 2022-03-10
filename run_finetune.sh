4774da06240a865f0ef69d45278eada6ca5b5bf0
Onur Galoglu <onur.galoglu@tum.de>
2022-03-10 16:30:51 +0100

    Edit typo

--- run_finetune.sh
+++ run_finetune.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 accelerate launch finetune.py \
-    --train_file /work/ogalolu/datasets/mldoc/en/train 
-    --validation_file /work/ogalolu/datasets/mldoc/en/train
+    --train_file /work/ogalolu/datasets/mldoc/en/train \
+    --validation_file /work/ogalolu/datasets/mldoc/en/validation \
     --pretrained_dir /home/ogalolu/thesis/trained_models/mbert_2_frozen_4__2022_03_10-15_38_36 \
     --per_device_train_batch_size 32 \
     --per_device_eval_batch_size 32 \
