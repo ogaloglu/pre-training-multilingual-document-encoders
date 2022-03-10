68e37271bf81ad45cae57574455db37793eb48d0
Onur Galoglu <onur.galoglu@tum.de>
2022-03-10 12:06:30 +0100

    Change train.sh

--- run_train.sh
+++ run_train.sh
@@ -3,21 +3,23 @@
 accelerate launch train.py \
     --train_file /work/ogalolu/datasets/final_small_en_0.3 \
     --model_name_or_path bert-base-multilingual-cased \
-    --per_device_train_batch_size 16 \
-    --per_device_eval_batch_size 16 \
-    --num_train_epochs 2\
+    --per_device_train_batch_size 32 \
+    --per_device_eval_batch_size 32 \
+    --num_train_epochs 4\
     --gradient_accumulation_steps 1 \
     --num_warmup_steps 3000 \
     --output_dir ../trained_models \
     --seed 42 \
     --max_seq_length 128 \
-    --preprocessing_num_workers 8 \
+    --preprocessing_num_workers 16 \
     --max_document_length 32 \
     --upper_nhead 8 \
     --upper_num_layers 2 \
     --frozen \
     --upper_positional \
-    --is_pretraining
+    --learning_rate 5e-5 \
+    --weight_decay 0.0 \
+    --is_contrastive
 
 
 # CUDA_VISIBLE_DEVICES = "1,2,3" accelerate launch train.py \
