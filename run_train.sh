5f22eac728ed7687e17e19bb62a8250d34c6c9c4
Onur Galoglu <onur.galoglu@gmail.com>
2022-03-20 12:09:21 +0100

    Update for new pooling options

--- run_train.sh
+++ run_train.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 accelerate launch train.py \
-    --train_file /work/ogalolu/datasets/final_small_en_0.3 \
-    --model_name_or_path bert-base-multilingual-cased \
+    --train_file /work/ogalolu/datasets/large_final_small_en_0.3\
+    --model_name_or_path sentence-transformers/LaBSE\
     --per_device_train_batch_size 32 \
     --per_device_eval_batch_size 32 \
     --num_train_epochs 4\
@@ -16,13 +16,20 @@ accelerate launch train.py \
     --upper_nhead 8 \
     --upper_num_layers 2 \
     --frozen \
-    --upper_positional \
     --learning_rate 1e-5 \
     --weight_decay 0.0 \
-    --logging_steps 100 \
+    --logging_steps 200 \
     --lower_dropout 0.1 \
     --is_contrastive \
+    --upper_activation relu \
+    --lr_scheduler_type linear\
+    --scale 20 \
+    --upper_pooling dcls \
     --inspect
+
+# upper-positional
+# use_hard_negatives
+
 
 # accelerate launch train.py \
 #     --train_file /work/ogalolu/datasets/final_small_en_0.3 \
