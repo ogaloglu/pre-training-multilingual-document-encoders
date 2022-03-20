a3c9f5eb53b1f09e54e738af893fa6ed444a2a84
Onur Galoglu <onur.galoglu@gmail.com>
2022-03-20 12:32:50 +0100

    Move modeling related methods to model_utils

--- utils.py
+++ utils.py
@@ -1,11 +1,9 @@
-""" Utility functions to be used while the training."""
+""" Utility functions to be for tokenization and handling arguments."""
 import argparse
 import os
 import json
 from collections import namedtuple
 
-import torch
-from torch import Tensor
 from nltk import sent_tokenize
 from datasets import arrow_dataset
 from transformers.modeling_outputs import SequenceClassifierOutput
@@ -16,30 +14,6 @@ MODEL_MAPPING = {
     "xlm-roberta-base": "xlmr",
     "sentence-transformers/LaBSE": "labse"
 }
-
-
-def cos_sim(a: Tensor, b: Tensor) -> Tensor:
-    """
-    Computes the cosine similarity cos_sim(a[i], b[j]) for all i and j.
-    :return: Matrix with res[i][j]  = cos_sim(a[i], b[j])
-
-    Taken from https://github.com/UKPLab/sentence-transformers/blob/master/sentence_transformers/util.py
-    """
-    if not isinstance(a, torch.Tensor):
-        a = torch.tensor(a)
-
-    if not isinstance(b, torch.Tensor):
-        b = torch.tensor(b)
-
-    if len(a.shape) == 1:
-        a = a.unsqueeze(0)
-
-    if len(b.shape) == 1:
-        b = b.unsqueeze(0)
-
-    a_norm = torch.nn.functional.normalize(a, p=2, dim=1)
-    b_norm = torch.nn.functional.normalize(b, p=2, dim=1)
-    return torch.mm(a_norm, b_norm.transpose(0, 1))
 
 
 def custom_tokenize(example: arrow_dataset.Example, tokenizer,
@@ -135,41 +109,3 @@ def sliding_tokenize(example: arrow_dataset.Example, args: argparse.Namespace, t
         "mask_1": sentences["attention_mask"],
         "labels": example["labels"]
     }
-
-
-def freeze_base(model):
-    # For freezing base of the auto_models.
-    for param in model.base_model.parameters():
-        param.requires_grad = False
-
-
-def get_extended_attention_mask(attention_mask: Tensor) -> Tensor:
-    """
-    Makes broadcastable attention and causal masks so that future and masked tokens are ignored.
-    Arguments:
-        attention_mask (`torch.Tensor`):
-            Mask with ones indicating tokens to attend to, zeros for tokens to ignore.
-    Returns:
-        `torch.Tensor` The extended attention mask, with a the same dtype as `attention_mask.dtype`.
-    """
-    # We can provide a self-attention mask of dimensions [batch_size, to_seq_length]
-    # ourselves in which case we just need to make it broadcastable to all heads.
-    extended_attention_mask = attention_mask[:, None, None, :]
-    extended_attention_mask = extended_attention_mask.to(dtype=extended_attention_mask.dtype)  # fp16 compatibility
-    extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
-    return extended_attention_mask
-
-
-def get_mean(upper_output: Tensor, document_mask: Tensor) -> Tensor:
-
-    # print(upper_output)
-    # document_mask has to be expanded to the shape of upper_output
-    input_mask_expanded = document_mask.unsqueeze(-1).expand(upper_output.size()).float()
-
-    # Rather than taking simply mean, we have to consider the padded documents.
-    # Therefore, effective length of each document will also change accordingly.
-    sum_embeddings = torch.sum(upper_output * input_mask_expanded, 1)
-    sum_mask = input_mask_expanded.sum(1)
-    sum_mask = torch.clamp(sum_mask, min=1e-9)
-    output = sum_embeddings / sum_mask
-    return output
