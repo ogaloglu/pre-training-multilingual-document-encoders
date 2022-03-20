b16d25fb90b44e2f3fd09f6418e05e7aff4d1812
Onur Galoglu <onur.galoglu@gmail.com>
2022-03-20 12:15:48 +0100

    Reorganize parameters

--- run_train.sh
+++ run_train.sh
@@ -2,29 +2,30 @@
 
 accelerate launch train.py \
     --train_file /work/ogalolu/datasets/large_final_small_en_0.3\
+    --output_dir ../trained_models \
+    --seed 42 \
     --model_name_or_path sentence-transformers/LaBSE\
     --per_device_train_batch_size 32 \
     --per_device_eval_batch_size 32 \
-    --num_train_epochs 4\
     --gradient_accumulation_steps 1 \
-    --num_warmup_steps 3000 \
-    --output_dir ../trained_models \
-    --seed 42 \
-    --max_seq_length 128 \
     --preprocessing_num_workers 32 \
+    --max_seq_length 128 \
     --max_document_length 32 \
-    --upper_nhead 8 \
-    --upper_num_layers 2 \
-    --frozen \
+    --num_train_epochs 4\
+    --num_warmup_steps 3000 \
     --learning_rate 1e-5 \
+    --lr_scheduler_type linear\
     --weight_decay 0.0 \
     --logging_steps 200 \
+    --frozen \
     --lower_dropout 0.1 \
-    --is_contrastive \
-    --upper_activation relu \
-    --lr_scheduler_type linear\
-    --scale 20 \
+    --upper_nhead 8 \
+    --upper_num_layers 2 \
+    --upper_activation gelu \
     --upper_pooling dcls \
+    --upper_dim_feedforward 2048\
+    --scale 20 \
+    --is_contrastive \
     --inspect
 
 # upper-positional
