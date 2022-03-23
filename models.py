6df411578ec38894b87e461aa2dde3b92ffec9f5
Onur Galoglu <onur.galoglu@gmail.com>
2022-03-24 00:15:35 +0100

    Update ClassificationModel according to changes in hierarchicalModel

--- models.py
+++ models.py
@@ -268,8 +268,12 @@ class HierarchicalClassificationModel(nn.Module):
 
         self.classifier = nn.Linear(self.hierarchical_model.lower_config.hidden_size, self.num_labels)
 
-    def forward(self, article_1, mask_1, labels):
-        output = self.hierarchical_model(input_ids=article_1, attention_mask=mask_1)  # (batch_size, hidden_size)
+    def forward(self, article_1, mask_1, dcls_1, document_mask_1, labels):
+        output = self.hierarchical_model(input_ids=article_1,
+                                           attention_mask=mask_1,
+                                           dcls=dcls_1,
+                                           document_mask=document_mask_1
+                                           )  # (batch_size, hidden_size)
 
         output = self.dropout(output)
         logits = self.classifier(output)
