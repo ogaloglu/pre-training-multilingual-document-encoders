70545fb089e6f732ffdceea6f86d4615033a54ba
Onur Galoglu <onur.galoglu@gmail.com>
2022-04-25 19:29:05 +0200

    Add utilities for longformer

--- model_utils.py
+++ model_utils.py
@@ -1,9 +1,12 @@
 """ Utility functions to be used for modeling."""
+import copy
 from dataclasses import dataclass
 
 import torch
 from torch import Tensor
 from transformers.file_utils import ModelOutput
+from transformers import XLMRobertaForMaskedLM, BertForMaskedLM, RobertaForMaskedLM, PreTrainedModel, BertModel, RobertaModel, XLMRobertaModel, AutoTokenizer, AutoModelForMaskedLM
+from transformers.models.longformer.modeling_longformer import LongformerSelfAttention
 
 
 def cos_sim(a: Tensor, b: Tensor) -> Tensor:
@@ -89,3 +92,99 @@ class ContrastiveModelOutput(ModelOutput):
     dist_1: torch.Tensor = None
     scores_2: torch.Tensor = None
     dist_2: torch.Tensor = None
+
+
+def pretrained_masked_model_selector(seed_model: str) -> PreTrainedModel: 
+    if seed_model == "xlm-roberta-base":
+        PRETRAINED_MODEL = XLMRobertaForMaskedLM
+    elif seed_model == "roberta-base":
+        PRETRAINED_MODEL = RobertaForMaskedLM
+    elif seed_model == "sentence-transformers/LaBSE":
+        PRETRAINED_MODEL = BertForMaskedLM
+    else:
+        raise NotImplementedError(
+        "Other models are not supported")
+    return PRETRAINED_MODEL
+
+
+def pretrained_model_selector(seed_model: str) -> PreTrainedModel: 
+    if seed_model == "xlm-roberta-base":
+        PRETRAINED_MODEL = XLMRobertaModel
+    elif seed_model == "roberta-base":
+        PRETRAINED_MODEL = RobertaModel
+    elif seed_model == "sentence-transformers/LaBSE":
+        PRETRAINED_MODEL = BertModel
+    else:
+        raise NotImplementedError(
+        "Other models are not supported")
+    return PRETRAINED_MODEL
+
+
+import copy
+import torch
+
+from transformers import XLMRobertaForMaskedLM, BertForMaskedLM, RobertaForMaskedLM, PreTrainedModel, BertModel, RobertaModel, XLMRobertaModel, AutoTokenizer, AutoModelForMaskedLM
+from transformers.models.longformer.modeling_longformer import LongformerSelfAttention
+
+def create_long_model(seed_model, save_model_to, attention_window, max_pos):
+    """Modified: all roberta -> base_model."""
+
+    # MODIFIED: 
+    model = AutoModelForMaskedLM.from_pretrained(seed_model)
+    tokenizer = AutoTokenizer.from_pretrained(seed_model, model_max_length=max_pos, use_fast=True)
+    config = model.config
+
+    # extend position embeddings
+    tokenizer.model_max_length = max_pos
+    tokenizer.init_kwargs['model_max_length'] = max_pos
+    current_max_pos, embed_size = model.base_model.embeddings.position_embeddings.weight.shape
+    if seed_model in ("xlm-roberta-base", "roberta-base"):
+        max_pos += 2  # NOTE: RoBERTa has positions 0,1 reserved, so embedding size is max position + 2
+        assert max_pos > current_max_pos
+    config.max_position_embeddings = max_pos
+    # allocate a larger position embedding matrix
+    new_pos_embed = model.base_model.embeddings.position_embeddings.weight.new_empty(max_pos, embed_size)
+    # copy position embeddings over and over to initialize the new position embeddings
+    
+    # MODIFIED: 
+    if seed_model in ("xlm-roberta-base", "roberta-base"):
+        k = 2
+    else:
+        k = 0
+    step = current_max_pos - k
+    while k < max_pos - 1:
+        if seed_model in ("xlm-roberta-base", "roberta-base"):
+            new_pos_embed[k:(k + step)] = model.base_model.embeddings.position_embeddings.weight[k:]
+        else:
+            new_pos_embed[k:(k + step)] = model.base_model.embeddings.position_embeddings.weight[:]
+        # END OF MODIFICATION
+        k += step
+    model.base_model.embeddings.position_embeddings.weight.data = new_pos_embed
+    model.base_model.embeddings.position_ids.data = torch.tensor([i for i in range(max_pos)]).reshape(1, max_pos)  
+    
+    # replace the `modeling_bert.BertSelfAttention` object with `LongformerSelfAttention`
+    config.attention_window = [attention_window] * config.num_hidden_layers
+    for i, layer in enumerate(model.base_model.encoder.layer):
+        longformer_self_attn = LongformerSelfAttention(config, layer_id=i)
+        longformer_self_attn.query = layer.attention.self.query
+        longformer_self_attn.key = layer.attention.self.key
+        longformer_self_attn.value = layer.attention.self.value
+
+        longformer_self_attn.query_global = copy.deepcopy(layer.attention.self.query)
+        longformer_self_attn.key_global = copy.deepcopy(layer.attention.self.key)
+        longformer_self_attn.value_global = copy.deepcopy(layer.attention.self.value)
+
+        layer.attention.self = longformer_self_attn
+
+    print(f'saving model to {save_model_to}')
+    model.save_pretrained(save_model_to)
+    tokenizer.save_pretrained(save_model_to)
+    return model, tokenizer
+
+
+def copy_proj_layers(model):
+    for i, layer in enumerate(model.base_model.encoder.layer):
+        layer.attention.self.query_global = copy.deepcopy(layer.attention.self.query)
+        layer.attention.self.key_global = copy.deepcopy(layer.attention.self.key)
+        layer.attention.self.value_global = copy.deepcopy(layer.attention.self.value)
+    return model
