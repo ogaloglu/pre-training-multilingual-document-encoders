4d4a89039ad48cd1edb82bcb578bbea7de602200
Onur Galoglu <onur.galoglu@gmail.com>
2023-02-16 23:30:39 +0100

    Change project structure by moving shell scripts

--- run_clef_dual_encoder.sh
+++ run_clef_dual_encoder.sh
@@ -5,11 +5,6 @@ GPU=7
 
 # Directory containing a trained monoBERT model
 
-# MODEL_DIR=/work-ceph/ogalolu/models/finetuned_models/ms_marco/labse_contrastive2022_05_22-12_35_31/
-# CUSTOM_MODEL=hierarchical
-# PRETRAINED_EPOCH=5000
-# python clef/monobert_eval.py --model_dir $MODEL_DIR --prerank_dir $PRERANKING_DIR --mode clir --gpu $GPU --custom_model $CUSTOM_MODEL --pretrained_epoch $PRETRAINED_EPOCH
-
 MODEL_DIR=/work-ceph/ogalolu/models/finetuned_models/clef/labse_contrastive2022_06_05-17_56_41/
 CUSTOM_MODEL=hierarchical
 python clef/monobert_eval.py --model_dir $MODEL_DIR --prerank_dir $PRERANKING_DIR --mode clir --gpu $GPU --custom_model $CUSTOM_MODEL --dual_encoder
