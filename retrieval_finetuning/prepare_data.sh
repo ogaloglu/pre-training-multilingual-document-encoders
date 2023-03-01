1b9a0355240dd058c63d34ef3a12daa112d80c01
Onur Galoglu <onur.galoglu@gmail.com>
2023-03-01 21:03:09 +0100

    Remove data prep files for msmarco

--- retrieval_finetuning/prepare_data.sh
+++
@@ -1,7 +0,0 @@
-PROJECT_HOME=/home/ogalolu/thesis/pre-training-multilingual-document-encoders/
-
-# Specify output directory for downloading and storing prepared data files.
-OUTPUT_DIR=/work/ogalolu/data/msmarco/
-mkdir -p $OUTPUT_DIR
-
-python $PROJECT_HOME/ms_marco/prepare_msmarco.py --output_dir $OUTPUT_DIR
